cff01a8f5fc27984b4acd3821b8fd3a6d31d6dbf
Shankar Amara <Shankar_Amara@epam.com>
2019-11-11 20:33:04 +0530

    commited ReverseWordsInStatement program

---
+++ LogicalAnalyticalApps/ReverseWordsInStatement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReverseWordsInStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a statement");
+            string inputLine = Console.ReadLine();
+            string[] strArray = inputLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            String[] output = new string[strArray.Length];
+            Int32 forward = 0;
+            for (Int32 i = strArray.Length - 1; i >= 0; i--)
+            {
+                output[forward++] = strArray[i];
+            }
+            Console.WriteLine($"Statement in reverse is: {string.Join(" ", output)}");
+            Console.ReadKey();
+        }
+    }
+}
